e590b3d8e95a5f6ecd492bb8d80653f3c15d2330
miguel <miguel.rueda@outlook.com>
2021-03-10 13:23:37 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Route, Router } from "svelte-routing";
+  import Detail from "./detail/Detail.svelte";
+  import Library from "./library/Library.svelte";
+
+  export let url = "";
+</script>
+
+<Router {url}>
+  <main>
+    <Route path="/books/:id" let:params>
+      <Detail id={params.id} />
+    </Route>
+    <Route path="/">
+      <Library />
+    </Route>
+  </main>
+</Router>
+
+<style>
+  main {
+    padding: var (--spacingLarge);
+  }
+</style>
